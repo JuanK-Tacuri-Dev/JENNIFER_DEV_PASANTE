97620524a6ab7bacd4f03bed7b8357dddee934d6
Jenifer Natalia <jennialvarez114@gmail.com>
2025-01-24 12:51:37 -0500

    Models, producto

---
+++ Infraestructura/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Infraestructura.Models
+{
+    public class Producto
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProveedorId { get; set; } // Relación con Proveedor
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ImpuestoId { get; set; } // Relación con Impuesto
+    }
+}
+
