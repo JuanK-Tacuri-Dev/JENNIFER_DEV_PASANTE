6d01e3e41ef8fb48e99b07ec0b6044dc708a1af6
Jenifer Natalia <jennialvarez114@gmail.com>
2025-01-24 11:52:51 -0500

    infraestructura, persistencia

---
+++ Infraestructura/Persistence/MongoDbContext.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+
+namespace Infraestructura.Persistence
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(string connectionString, string databaseName)
+        {
+            // Crear el cliente y conectarse a MongoDB
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        // Método para obtener una colección de la base de datos
+        public IMongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return _database.GetCollection<T>(collectionName);
+        }
+    }
+}
+
+
